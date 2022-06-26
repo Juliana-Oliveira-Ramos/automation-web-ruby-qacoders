521a284597b707a5350a0a78dd4c14981aabf0ab
Juliana-Oliveira-Ramos <julianaoliveiramos1993@gmail.com>
2022-06-25 21:54:29 -0300

    Configure env.rb file

---
+++ features/support/env.rb
@@ -0,0 +1,8 @@
+## (env.rb) primeiro é o arquivo a ser lido primeiro antes de excutar o projeto
+##chama todas as dependencias do projeto
+
+require 'capybara/cucumber'
+require 'capybara/rspec'
+require 'selenium-webdriver'
+require 'site_prism'
+require 'pry'
